f146e011f876a7650061c978a668da0ce77d6f17
DavidMMata <davidmmata89@gmail.com>
2023-04-22 01:41:14 -0600

    Added sliders

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  heigh: 30px;
+  background-color: teal;
+  color: white;
+  display: felx;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+export const Announcement = () => {
+  return <Container>ANNOUNCEMENTS</Container>;
+};
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://upload.wikimedia.org/wikipedia/commons/5/51/Dog_Silhouette_01.svg",
+    title: "SUMMER SALE",
+    desc: "They took er jorbs! Open up a can of whoop a$$ on those God darn Messicans!!!!",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://upload.wikimedia.org/wikipedia/commons/5/51/Dog_Silhouette_01.svg",
+    title: "FALL SALE",
+    desc: "GOD DARN COONS!!!!",
+    bg: "f5f1ed",
+  },
+  {
+    id: 3,
+    img: "https://upload.wikimedia.org/wikipedia/commons/5/51/Dog_Silhouette_01.svg",
+    title: "WINTER SALE",
+    desc: "LUNA BALLONA!!!!",
+    bg: "f5f1ed",
+  },
+];
